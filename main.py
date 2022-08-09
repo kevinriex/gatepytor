e67d2b3a3cab48ff2345f658806c89ddd29141ee
Kevin Riexinger <info@kevinriexinger.de>
2022-08-10 00:56:58 +0200

    half working // Project Abort

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from socket import timeout
 from torpy import TorClient
 from torpy.utils import recv_all
+from torpy.http import requests
+from torpy.http.adapter import TorHttpAdapter
 
-def main():
-    host = "http://facebookcorewwwi.onion"
-    host = "ifconfig.me"
+def socket():
     with TorClient() as tor:
         with tor.create_circuit(3) as circ:
             with circ.create_stream((host,80)) as stream:
@@ -11,6 +12,25 @@ def main():
                 ret = recv_all(stream).decode()
                 print(ret)
 
+def request():
+    with TorClient() as tor:
+        with tor.get_guard() as guard:
+            adapter = TorHttpAdapter(guard, 3)
+            with requests.Session() as session:
+                session.headers.update({'User-Agent':'Mozilla/5.0'})
+                session.mount('http://', adapter)
+                session.mount('https://', adapter)
+
+                resp = session.get(host, timeout=15)
+                print(resp.text)
+
+                resp = session.get(host2, timeout=15)
+                print(resp.text)
+
+
 
 if __name__ == "__main__":
-    main()
+    host = "http://facebookcorewwwi.onion"
+    host2 = "http://ifconfig.me"
+    #socket()
+    request()
